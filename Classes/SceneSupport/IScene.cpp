ab554c8ddec0f754c9ca6ccf0ca4ca0ae31a9078
コバヤシヒロタカ <kob.dango.kiddo@gmail.com>
2015-03-21 20:03:32 +0900

    2015/3/21 前回の製作で作った、使うであろうファイルを移植

---
+++ Classes/SceneSupport/IScene.cpp
@@ -0,0 +1,11 @@
+﻿#include "IScene.h"
+
+
+IScene::IScene() :mSceneState(FADEIN)
+{
+	updateFunc[0] = &IScene::fadeIn;
+	updateFunc[1] = &IScene::sceneMain;
+	updateFunc[2] = &IScene::fadeOut;
+	updateFunc[3] = nullptr;
+
+}
---
+++ Classes/Utility/Animation/SpriteAnimation.cpp
@@ -0,0 +1,36 @@
+﻿#include "SpriteAnimation.h"
+
+using namespace Util;
+
+SpriteAnimation::SpriteAnimation(const std::string& path){
+	m_ResourcePath = path;
+}
+
+
+Sprite* SpriteAnimation::create(const std::string& spriteName, const int count, const float delay, const bool restart, const bool repeat){
+	
+	std::string r_spriteName = spriteName;
+
+	Sprite* r_firstSprite = Sprite::create(m_ResourcePath + r_spriteName + "1.png");
+	r_firstSprite->setName(spriteName);
+
+	auto r_animation = Animation::create();
+	for (int i = 2; i <= count; ++i){
+		r_animation->addSpriteFrameWithFileName(m_ResourcePath + r_spriteName + std::to_string(i) + ".png");
+	}
+	r_animation->setRestoreOriginalFrame(restart);
+	r_animation->setDelayPerUnit(delay);
+
+	//繰り返すなら
+	if (repeat){
+		Animate* animate = Animate::create(r_animation);
+		RepeatForever* repeatTrue = RepeatForever::create(animate);
+		r_firstSprite->runAction(repeatTrue);
+	}
+
+	return r_firstSprite;
+}
+
+void SpriteAnimation::setResourcesPath(const std::string& path){
+	m_ResourcePath = path;
+}
