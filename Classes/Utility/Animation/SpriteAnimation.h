f597e1780595c6a022c414b3db4b65b0feffa214
コバヤシヒロタカ <kob.dango.kiddo@gmail.com>
2015-03-21 20:03:32 +0900

    2015/3/21 前回の製作で作った、使うであろうファイルを移植

---
+++ Classes/Utility/Animation/SpriteAnimation.h
@@ -0,0 +1,45 @@
+﻿#ifndef SPRITEANIMATION_H
+#define SPRITEANIMATION_H
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+#include <vector>
+#include <string>
+
+
+namespace Util{
+
+/*-----------------------------------------------------
+		cocos2dxのAnimationクラスを使用した
+		パラパラアニメーション作成クラス
+		ファイル名　SpriteAnimation.h
+		作成者　小林　大峰
+-----------------------------------------------------*/	
+class SpriteAnimation{
+public:
+	SpriteAnimation(const std::string& path);
+	~SpriteAnimation() = default;
+
+	/*--------------------------------------------------------------------------------
+		生成関数
+		string spriteName 画像の名前
+		int	   count	　フレーム数
+		float  delay      切り替え間隔
+		bool   restart	　一番初めの画像に戻すか
+		bool   repeat	　繰り返すか？
+	--------------------------------------------------------------------------------*/
+	Sprite* create(const std::string& spriteName, const int count, const float delay, const bool restart, const bool repeat);
+	
+
+	/*------------------------------------------------------------
+		リソースフォルダのパスを変更する
+		const std::string& path リソースパス
+	-------------------------------------------------------------*/
+	void setResourcesPath(const std::string& path);
+private:
+	std::string m_ResourcePath;
+};
+}
+
+#endif
