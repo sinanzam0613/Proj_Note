207090cd35ac3159535c4a1410281735b2cfc7f7
kobayashimega <s91415@infoseek.jp>
2015-03-27 14:31:33 +0900

    2015/3/27 ジャンプ確認のためのテストオブジェクトを追加

---
+++ Classes/GamePlay/Character/Test/TestObject.cpp
@@ -0,0 +1,28 @@
+#include "TestObject.h"
+#include "../SpriteCreator.h"
+
+using namespace cocos2d;
+
+TestObject::TestObject()
+{
+}
+
+TestObject::~TestObject()
+{
+}
+
+bool TestObject::init()
+{
+	if (!Node::init())
+	{
+		return false;
+	}
+	mSprite = SpriteCreator::create("GamePlay/Character/Test.png");
+	this->addChild(mSprite);
+	return true;
+}
+
+void TestObject::update(float deltaTime)
+{
+
+}
---
+++ Classes/GamePlay/Character/Test/TestObject.h
@@ -0,0 +1,24 @@
+#ifndef _TESTOBJECT_H_
+#define _TESTOBJECT_H_
+
+#include "cocos2d.h"
+
+
+/********************************************
+プレイヤーの取り付くテストオブジェクト
+後で消します
+作成者　小林勇輝
+*********************************************/
+class TestObject:public cocos2d::Node
+{
+public:
+	TestObject();
+	~TestObject();
+	virtual bool init()override;
+	virtual void update(float deltaTime)override;
+	CREATE_FUNC(TestObject);
+private:
+	cocos2d::Sprite* mSprite;
+};
+
+#endif
