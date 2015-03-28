962ca9cd11612be3f2203498645efaa4d6f095c8
kobayashimega <s91415@infoseek.jp>
2015-03-28 15:45:10 +0900

    2015/3/28 他ノードとの距離チェッククラスとジャンプの実装
    とりあえずアクションでジャンプを実装しました

---
+++ Classes/GamePlay/Character/DistanceCheck.h
@@ -0,0 +1,16 @@
+#ifndef _DISTANCECHECK_H_
+#define _DISTANCECHECK_H_
+
+#include "cocos2d.h"
+
+class DistanceCheck
+{
+public:
+	
+	static float Check(cocos2d::Node* myObject, cocos2d::Node* targetObject);
+
+private:
+
+};
+
+#endif
