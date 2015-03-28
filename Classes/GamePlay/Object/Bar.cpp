8f2677b9f1991c94f8042a6098787f6f56dafb49
コバヤシヒロタカ <kob.dango.kiddo@gmail.com>
2015-03-29 04:27:07 +0900

    3/29 ゴールとバーを仮追加

---
+++ Classes/GamePlay/Object/Bar.cpp
@@ -0,0 +1,32 @@
+﻿#include "Bar.h"
+
+USING_NS_CC;
+
+Bar* Bar::create(const std::string textureName){
+
+	auto obj = new Bar();
+
+	if (obj && obj->init(textureName)){
+
+		obj->autorelease();
+
+		return obj;
+	}
+
+	CC_SAFE_DELETE(obj);
+	return nullptr;
+
+}
+
+bool Bar::init(const std::string textureName){
+	mSprite = SpriteCreator::create(textureName);
+	mSprite->retain();
+
+	addChild(mSprite);
+
+	return true;
+}
+
+void Bar::update(float deltatime){
+
+}
