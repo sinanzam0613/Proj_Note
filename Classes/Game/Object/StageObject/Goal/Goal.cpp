57aea4216754c74d24b61be29447e3911e8e1dc3
利川聖太 <seita_012@yahoo.co.jp>
2015-06-12 11:17:04 +0900

    2015/06/12 ゴールにコリジョンを追加。

--- Classes/Game/Object/StageObject/Goal/Goal.cpp
+++ Classes/Game/Object/StageObject/Goal/Goal.cpp
@@ -19,14 +19,31 @@ Goal* Goal::create(const std::string textureName){
 }
 
 bool Goal::init(const std::string textureName){
-	mSprite = SpriteCreator::create(textureName);
-	mSprite->retain();
-
+	
+	mSprite = Sprite::create( textureName );
+	mSprite->setName( "Goal" );
 	addChild(mSprite);
+	
+	auto size = cocos2d::Size( mSprite->getContentSize().width, mSprite->getContentSize().height );
+	
+	mPhysicsBody = PhysicsBody::createBox( size, mPhysicsMaterial );
+	mPhysicsBody->setDynamic( true );
+	mPhysicsBody->setGravityEnable( false );
+	mPhysicsBody->setCategoryBitmask( 0xFFFFFFFF );
+	mPhysicsBody->setContactTestBitmask( 0xFFFFFFFF );
+	mPhysicsBody->setCollisionBitmask( 0xFFFFFFFF );
+	
+	mSprite->setPhysicsBody( mPhysicsBody );
+	this->enableCollision( mSprite->getName() );
 
 	return true;
 }
 
 void Goal::update(float deltatime){
 
+}
+
+void Goal::onContactBegin( Node* contactNode )
+{
+	Director::getInstance()->stopAnimation();
 }
--- Classes/Game/Object/StageObject/Goal/Goal.h
+++ Classes/Game/Object/StageObject/Goal/Goal.h
@@ -2,8 +2,9 @@
 #define GOAL_H
 
 #include "Game/Object/StageObject/StageObj.h"
+#include "Utility/Collision/Collider.h"
 
-class Goal : public StageObj {
+class Goal : public StageObj, public Collider {
 
 public:
 	static Goal* create(const std::string textureName);
@@ -11,6 +12,8 @@ public:
 	bool init(const std::string textureName);
 
 	void update(float deltatime) override;
+	
+	virtual void onContactBegin( cocos2d::Node* contactNode );
 
 protected:
 	Goal() = default;
