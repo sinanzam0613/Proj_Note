d34bd81aebf5bc1d73e245d94d56bc28ac640ea4
kobayashimega <s91415@infoseek.jp>
2015-06-22 11:29:38 +0900

    2015/6/22 ゴールクラスの追加

---
+++ Classes/Game/Object/Character/Player/GoalAnimation.cpp
@@ -0,0 +1,15 @@
+#include "GoalAnimation.h"
+
+GoalAnimation::GoalAnimation()
+{
+	mTimer.reset();
+}
+
+GoalAnimation::~GoalAnimation()
+{
+}
+
+void GoalAnimation::action(Player* playerRed, Player* playerBlue)
+{
+	
+}
