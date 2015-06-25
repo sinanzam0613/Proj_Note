ab00c48e30ac0f6f3c4af8a8eb69aa3fe2432746
kobayashimega <s91415@infoseek.jp>
2015-06-25 23:42:15 +0900

    2015/06/25 GoalAnimationの追加

--- Classes/Game/Object/Character/Player/GoalAnimation.h
+++ Classes/Game/Object/Character/Player/GoalAnimation.h
@@ -12,11 +12,17 @@ public:
 	GoalAnimation();
 	~GoalAnimation();
 
-	void action(Player* playerRed,Player* playerBlue);
+	void action(const cocos2d::Vec2& goalPosition ,cocos2d::Node* parentNode,cocos2d::Node* playerRed,cocos2d::Node* playerBlue);
+
+
+private:
+	void bellAnimation(const cocos2d::Vec2& goalPosition, cocos2d::Node* parentNode);
+	void jumpAnimation(cocos2d::Node* playerRed, cocos2d::Node* playerBlue);
+	void createEffect(cocos2d::Node* parent,cocos2d::Vec2 position);
 
 private:
 	MyUtil::Timer mTimer;
+	int mJumpCount;
 };
-
 
 #endif
