c33e8d3870d515406f0779759f5a1e4aff48c752
sinanzam0613 <kurotaka0613@hotmail.co.jp>
2015-06-26 13:04:49 +0900

    2015/06/26 Playerアニメ修正

--- Classes/Utility/Animation/SpriteAnimation.cpp
+++ Classes/Utility/Animation/SpriteAnimation.cpp
@@ -12,7 +12,7 @@ Sprite* SpriteAnimation::create(const std::string& spriteName, const int count, 
 	
 	std::string textureName = spriteName;
 
-	Sprite* firstSprite = Sprite::create(mResourcePath + textureName + "1.png");
+	firstSprite = Sprite::create(mResourcePath + textureName + "1.png");
 	firstSprite->setName(spriteName);
 
 	auto animation = Animation::create();
@@ -21,18 +21,37 @@ Sprite* SpriteAnimation::create(const std::string& spriteName, const int count, 
 	}
 	animation->setRestoreOriginalFrame(restart);
 	animation->setDelayPerUnit(delay);
-
+	Animate* animate = Animate::create(animation);
+	animate->setTag(1122);
 	//繰り返すなら
 	if (repeat){
-		Animate* animate = Animate::create(animation);
 		RepeatForever* repeatTrue = RepeatForever::create(animate);
 		firstSprite->runAction(repeatTrue);
 	} else {
-		Animate* animate = Animate::create(animation);
+
 		firstSprite->runAction(animate);
 	}
-
+	
 	return firstSprite;
+}
+
+Animate* SpriteAnimation::createAnim(const std::string& spriteName, const int count, const float delay, const bool restart){
+
+	std::string textureName = spriteName;
+
+	firstSprite = Sprite::create(mResourcePath + textureName + "1.png");
+	firstSprite->setName(spriteName);
+
+	auto animation = Animation::create();
+	for (int i = 2; i <= count; ++i){
+		animation->addSpriteFrameWithFile(mResourcePath + textureName + std::to_string(i) + ".png");
+	}
+	animation->setRestoreOriginalFrame(restart);
+	animation->setDelayPerUnit(delay);
+	Animate* animate = Animate::create(animation);
+	animate->setTag(1122);
+	//繰り返すなら
+	return animate;
 }
 
 void SpriteAnimation::setResourcesPath(const std::string& path){
